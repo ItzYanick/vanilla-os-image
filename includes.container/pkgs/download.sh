6842a58e063fa1917085d49c6fa19f251c3b33b3
Yanick Stephan <me@yanick.gay>
2024-12-23 16:37:14 +0100

    remove app image launcher because of missing binfmt

--- includes.container/pkgs/download.sh
+++ includes.container/pkgs/download.sh
@@ -2,4 +2,4 @@
 set -e
 
 # AppImage Support
-wget https://files.yanick.gay/os/linux/vanillaos/appimagelauncher_2.2.0_amd64.deb -P /pkgs
+# wget https://files.yanick.gay/os/linux/vanillaos/appimagelauncher_2.2.0_amd64.deb -P /pkgs
--- includes.container/pkgs/install.sh
+++ includes.container/pkgs/install.sh
@@ -3,4 +3,4 @@ set -e
 
 # AppImage Support
 apt-get install -y libfuse2t64
-apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
+# apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
