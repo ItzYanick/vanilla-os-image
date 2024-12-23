fd2d8a01796d3b60cda13b8137d993814346a4e8
Yanick Stephan <me@yanick.gay>
2024-12-23 18:19:34 +0100

    add displaylink driver

--- includes.container/pkgs/download.sh
+++ includes.container/pkgs/download.sh
@@ -3,3 +3,6 @@ set -e
 
 # AppImage Support
 # wget https://files.yanick.gay/os/linux/vanillaos/appimagelauncher_2.2.0_amd64.deb -P /pkgs
+
+# DisplayLink Support
+wget https://files.yanick.gay/os/linux/vanillaos/DisplayLink_6.1_amd64.zip -P /pkgs
