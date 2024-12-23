3f921d664802b0c6c946a2161a4355a6a3196b39
Yanick Stephan <me@yanick.gay>
2024-12-23 16:19:56 +0100

    Actual Initial commit

---
+++ includes.container/pkgs/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# AppImage Support
+wget https://files.yanick.gay/os/linux/vanillaos/appimagelauncher_2.2.0_amd64.deb -P /pkgs
---
+++ includes.container/pkgs/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+# AppImage Support
+apt-get install -y libfuse2t64
+apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
