203100e8174bc9ae877aeb2f1f5002cf964ee788
Yanick Stephan <me@yanick.gay>
2024-12-23 19:41:22 +0100

    Revert "add displaylink driver"
    This reverts commit fd2d8a01796d3b60cda13b8137d993814346a4e8.

--- includes.container/pkgs/install.sh
+++ includes.container/pkgs/install.sh
@@ -4,8 +4,3 @@ set -e
 # AppImage Support
 apt-get install -y libfuse2t64
 # apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
-
-# DisplayLink Support
-apt-get install -y dkms libdrm-dev
-unzip /pkgs/DisplayLink_6.1_amd64.zip -d /pkgs
-SYSTEMINITDAEMON=systemd bash /pkgs/displaylink-driver-6.1.0-17.run
